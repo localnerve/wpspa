5d53e1729711d965a5f8f6e20c7390ec5720362f
localnerve <alex@localnerve.com>
2013-10-02 15:33:54 -0400

    minor server module refactor

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -10,6 +10,36 @@ var injector = require("connect-injector");
 var proxy = require("./proxy");
 var notfound = require("./notfound");
 
+function addProxy(stack, options) {
+  if (options.proxy) {
+    stack.push(proxy(options.proxy.host, options.proxy.port, options.proxy.pattern));
+  }
+}
+
+function addInjector(stack, options) {
+  if (options.inject) {
+    stack.push(injector(options.inject.condition, options.inject.responder));
+  }
+}
+
+function addCompressor(stack, options) {
+  if (options.compress) {
+    stack.push(options.compress);
+  }
+}
+
+function addRewrites(stack, options) {
+  if (options.rewrite) {
+    stack.push(rewrite(options.rewrite));
+  }
+}
+
+function addStatic(stack, options) {
+  if (options.static) {
+    stack.push(options.static);
+  }
+}
+
 /*
  * Build the middleware stack
  * This controls the order of the middleware stack
@@ -25,32 +55,13 @@ function middleware(options) {
   options = options || {};
   var stack = [];
 
-  // add a proxy
-  if (options.proxy) {
-    stack.push(proxy(options.proxy.host, options.proxy.port, options.proxy.pattern));
-  }
+  addProxy(stack, options);
+  addInjector(stack, options);
+  addCompressor(stack, options);
+  addRewrites(stack, options);
+  addStatic(stack, options);
 
-  // add response injector
-  if (options.inject) {
-    stack.push(injector(options.inject.condition, options.inject.responder));
-  }
-
-  // add compressor
-  if (options.compress) {
-    stack.push(options.compress);
-  }
-
-  // add rewrite rules
-  if (options.rewrite) {
-    stack.push(rewrite(options.rewrite));
-  }
-
-  // add static files
-  if (options.static) {
-    stack.push(options.static);
-  }
-
-  // add 404
+  // always add 404 at the end
   stack.push(notfound.four04);
 
   return stack;
--- server/middleware/notfound.js
+++ server/middleware/notfound.js
@@ -5,23 +5,36 @@
 var fs = require("fs");
 var path = require("path");
 
+function serve404Html(res) {
+  // TODO: make the path part of config
+  var path404 = path.normalize(__dirname + "../../../../404.html");
+  fs.readFile(path404, { encoding: "utf8" }, function(e, html) {
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end(html);
+  });
+}
+
+function serve404Json(res) {
+  var json = "{ error: \"not found\" }";
+  res.setHeader("Content-Type", "application/json");
+  res.end(json);
+}
+
+function serve404Text(res) {
+  res.writeHead(res.statusCode, { "Content-Type": "text/plain" });
+  res.end(req.url + " was not found on this server");
+}
+
 function four04(req, res, next) {
   res.statusCode = 404;
   
-  var accept = req.headers.accept || '';
+  var accept = req.headers.accept || "";
   if (~accept.indexOf("html")) {
-    var path404 = path.normalize(__dirname + "../../../../404.html");
-    fs.readFile(path404, { encoding: "utf8" }, function(e, html) {
-      res.setHeader("Content-Type", "text/html; charset=utf-8");
-      res.end(html);
-    });
+    serve404Html(res);
   } else if (~accept.indexOf("json")) {
-    var json = "{ error: \"not found\" }";
-    res.setHeader("Content-Type", "application/json");
-    res.end(json);
+    serve404Json(res);
   } else {
-    res.writeHead(res.statusCode, { "Content-Type": "text/plain" });
-    res.end(req.url + " was not found on this server");
+    serve404Text(res);
   }
 }
 
