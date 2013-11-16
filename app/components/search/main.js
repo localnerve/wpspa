3b7f69049d2bfcbb20fa4889f01057a5a57af809
localnerve <alex@localnerve.com>
2013-11-16 09:22:56 -0500

    improved usability of search forms

--- app/components/search/main.js
+++ app/components/search/main.js
@@ -18,19 +18,21 @@ define([
       app.vent.trigger("container:complete");
     });
 
+    // The main submit handler for search all form submissions
+    function formSubmit(event) {
+      var $searchField = $(event.currentTarget).find("input.search-field");
+      var query = $searchField.prop("value");
+      if (query && query.length > 0) {
+        $searchField.prop("value", "").blur();
+        Backbone.history.navigate("search/"+query, true);
+        return false;
+      }
+    }
+
     // Add an initializer to the search module
     search.addInitializer(function() {
-
-      // Define a search event handler for form submissions (calls the controller via a route)
-      this.formSubmit = function(event) {
-        var searchField = $(event.currentTarget).find("input.search-field");
-        var query = searchField.prop("value");
-        if (query && query.length > 0) {
-          Backbone.history.navigate("search/"+query, true);
-          return false;
-        }
-      };
-
+      // expose the search form submit handler
+      this.formSubmit = formSubmit;
     });
 
   });
