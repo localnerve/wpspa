d9d1687393c2625eb13ab74ed228e148090074f5
localnerve <alex@localnerve.com>
2014-02-01 17:57:26 -0500

    fixes #47, collection get_posts filter

--- app/components/content/entities/collection.js
+++ app/components/content/entities/collection.js
@@ -28,10 +28,17 @@ define([
     url: function() {
       // if fetch items are specified, get the specific items
       if (this.options.items && this.options.items.length > 0) {
-        var urlRoot = urls.normalizeUrlRoot(this.urlRoot);
-        var object_type = types.baseObjectType(this.options.object_type);
-        var object_ids = _.pluck(this.options.items, "object_id");
-        return urlRoot + "?post_type="+object_type+"&post__in("+object_ids.join(",")+")"+"&custom_fields=_wpspa_meta_description,_wpspa_page_title";
+        var index = 0;
+
+        // create the post__in WP_Query filter for these items, URI encoded
+        var fetchIds = _.map(this.options.items, function(value) {
+          return "post__in%5B"+(index++)+"%5D="+value.object_id;
+        });
+
+        return urls.normalizeUrlRoot(this.urlRoot) +
+               "?post_type="+types.baseObjectType(this.options.object_type) +
+               "&"+fetchIds.join("&") +
+               "&custom_fields=_wpspa_meta_description,_wpspa_page_title";
       } else {
         return module.config().endpoint;
       }
