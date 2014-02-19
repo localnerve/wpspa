1a0523007af6ab90a4546d9a1f0497966df5732b
localnerve <alex@localnerve.com>
2014-02-18 21:30:11 -0500

    production config derive from release config

--- server/config/production.js
+++ server/config/production.js
@@ -2,32 +2,32 @@
  * production.js
  *
  * production configuration settings
+ *
+ * IMPORTANT: Do not add any application consumed values here
+ *   Only add values that affect/reflect server operational characteristics
+ *   Production application values must be added to config/release.js
  */
 var util = require("util");
-var All = require("./all");
+var Release = require("./release");
 
 function Config() {}
-util.inherits(Config, All);
+util.inherits(Config, Release);
 
 // Define the app host as the name for the cloud env router
 // The application info
 Config.prototype.app = {
-  hostname: "enigmatic-refuge-9006.herokuapp.com"
-  //port: undefined, let the env define
+  hostname: "enigmatic-refuge-9006.herokuapp.com",
+  port: undefined // must be defined by the real host environment
 };
 
 // Reduce production logging to minimal
 Config.prototype.loggerFormat = "tiny";
 
+Config.prototype.staticBase = ".";
+
 // Define the far-future expiry of statics (in milliseconds)
 Config.prototype.staticAge = 31556926000; // one year
 
-// Use a proxy
-Config.prototype.proxy = {
-  hostname: "wpspa.localnerve.com",
-  port: 80,
-  pattern: '^\/api\/'
-};
 
 // add more development properties/functions here
 
